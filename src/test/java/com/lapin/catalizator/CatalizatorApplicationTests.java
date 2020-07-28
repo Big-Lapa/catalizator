23bcc9f65153368ac8f03e68ba9999623b3d7cd0
Big_Lapa <andreylapin95@gmail.com>
2020-07-28 13:59:39 +0300

    Initial commit

---
+++ src/test/java/com/lapin/catalizator/CatalizatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lapin.catalizator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalizatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
